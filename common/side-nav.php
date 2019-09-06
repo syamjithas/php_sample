f6507e8a3ca0edc04ae2af067c2709bc1ab12468
Karthika Vijayan <karthikavijayan26jan@gmail.com>
2019-09-06 20:26:08 +0400

    side nav remove unwanted items

--- common/side-nav.php
+++ common/side-nav.php
@@ -26,9 +26,9 @@ $user_auth  = mysqli_fetch_array($result);
                 <?php
                 if ($user['gender'] == 'M') {
 
-                    echo "<img class='img-responsive img-rounded' src='".urlBase()."/image/male.png' alt='User picture'>";
+                    echo "<img class='img-responsive img-rounded' src='" . urlBase() . "/image/male.png' alt='User picture'>";
                 } else {
-                    echo "<img class='img-responsive img-rounded' src='".urlBase()."/image/female.jpg' alt='User picture'>";
+                    echo "<img class='img-responsive img-rounded' src='" . urlBase() . "/image/female.jpg' alt='User picture'>";
                 } ?>
             </div>
             <div class="user-info">
@@ -38,143 +38,19 @@ $user_auth  = mysqli_fetch_array($result);
                     <?php echo ucwords($user['last_name']); ?>
                 </span>
                 <span class="user-role">
-                <?php echo ucwords($user_auth['role']); ?>
+                    <?php echo ucwords($user_auth['role']); ?>
                 </span>
-                <!-- <span class="user-status">
-                    <i class="fa fa-circle"></i>
-                    <span>Online</span>
-                </span> -->
-            </div>
-        </div>
 
-        <div class="sidebar-search">
-            <div>
-                <div class="input-group">
-                    <input type="text" class="form-control search-menu" placeholder="Search...">
-                    <div class="input-group-append">
-                        <span class="input-group-text">
-                            <i class="fa fa-search" aria-hidden="true"></i>
-                        </span>
-                    </div>
-                </div>
             </div>
         </div>
 
         <div class="sidebar-menu">
             <ul>
-                <li class="header-menu">
-                    <span>General</span>
-                </li>
-                <li class="sidebar-dropdown">
-                    <a href="#">
-                        <i class="fa fa-tachometer-alt"></i>
-                        <span>Dashboard</span>
-                        <span class="badge badge-pill badge-warning">New</span>
-                    </a>
-                    <div class="sidebar-submenu">
-                        <ul>
-                            <li>
-                                <a href="#">Dashboard 1
-                                    <span class="badge badge-pill badge-success">Pro</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">Dashboard 2</a>
-                            </li>
-                            <li>
-                                <a href="#">Dashboard 3</a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-                <li class="sidebar-dropdown">
-                    <a href="#">
-                        <i class="fa fa-shopping-cart"></i>
-                        <span>E-commerce</span>
-                        <span class="badge badge-pill badge-danger">3</span>
-                    </a>
-                    <div class="sidebar-submenu">
-                        <ul>
-                            <li>
-                                <a href="#">Products
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">Orders</a>
-                            </li>
-                            <li>
-                                <a href="#">Credit cart</a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-                <li class="sidebar-dropdown">
-                    <a href="#">
-                        <i class="far fa-gem"></i>
-                        <span>Components</span>
-                    </a>
-                    <div class="sidebar-submenu">
-                        <ul>
-                            <li>
-                                <a href="#">General</a>
-                            </li>
-                            <li>
-                                <a href="#">Panels</a>
-                            </li>
-                            <li>
-                                <a href="#">Tables</a>
-                            </li>
-                            <li>
-                                <a href="#">Icons</a>
-                            </li>
-                            <li>
-                                <a href="#">Forms</a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-                <li class="sidebar-dropdown">
-                    <a href="#">
-                        <i class="fa fa-chart-line"></i>
-                        <span>Charts</span>
-                    </a>
-                    <div class="sidebar-submenu">
-                        <ul>
-                            <li>
-                                <a href="#">Pie chart</a>
-                            </li>
-                            <li>
-                                <a href="#">Line chart</a>
-                            </li>
-                            <li>
-                                <a href="#">Bar chart</a>
-                            </li>
-                            <li>
-                                <a href="#">Histogram</a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-                <li class="sidebar-dropdown">
-                    <a href="#">
-                        <i class="fa fa-globe"></i>
-                        <span>Maps</span>
-                    </a>
-                    <div class="sidebar-submenu">
-                        <ul>
-                            <li>
-                                <a href="#">Google maps</a>
-                            </li>
-                            <li>
-                                <a href="#">Open street map</a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
+
                 <li class="header-menu">
                     <span>Extra</span>
                 </li>
-                <li>
+                <li class="sidebar-menu-item active">
                     <a href="#">
                         <i class="fa fa-book"></i>
                         <span>Documentation</span>
