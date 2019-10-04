80dee8dbd5ff62345ee61dec13c6f12d57d88b88
syamjithas <syamjithas@gmail.com>
2019-10-04 12:10:11 +0400

    new script

--- db_script/script.sql
+++ db_script/script.sql
@@ -24,3 +24,14 @@ AUTO_INCREMENT=3;
 
 
 INSERT INTO aadhaar_dump (aadhaar_num) VALUES (LPAD(FLOOR(RAND() * 999999999999.99), 12, '0'));
+
+UPDATE aadhaar_dump SET aadhaar_num = LPAD(FLOOR(RAND() * 999999999999.99), 12, '0')
+WHERE aadhaar_num ='124354454545' DECLARE i = LPAD(FLOOR(RAND() * 999999999999.99), 12, '0');
+INSERT INTO aadhaar_dump (aadhaar_num) VALUES (i) SET @K = 0;
+ lab1: REPEAT
+INSERT INTO aadhaar_dump (aadhaar_num) VALUES ('12313123213'); SET @k = @k +1; UNTIL @K > 100 END REPEAT lab1;
+
+CREATE PROCEDURE dorepeat(p1 INT) BEGIN SET @x = 0; REPEAT SET @x = @x + 1; UNTIL @x > p1 END REPEAT; END 
+CALL dorepeat(1000)//
+
+DELETE FROM aadhaar_dump WHERE aadhaar_num LIKE '0%'
